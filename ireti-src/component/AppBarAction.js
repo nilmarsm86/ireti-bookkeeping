8e6fe695367d063b1d3da558a753dc613e3b8361
Nilmar Sanchez Muguercia <nilmarsm86@gmail.com>
2024-05-29 08:15:30 -0400

    optimizations

--- ireti-src/component/AppBarAction.js
+++ ireti-src/component/AppBarAction.js
@@ -1,7 +1,7 @@
 import { Appbar, Badge, Tooltip } from "react-native-paper";
 
 const AppBarAction = ({ title, icon, amount = null, onPress, active }) => {
-  title = amount > 0 ? title + " " + amount : title;
+  //title = amount > 0 ? title + " " + amount : title;
 
   return (
     <>
